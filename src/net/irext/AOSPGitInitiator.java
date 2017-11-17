4ccbd1fd77e0859de42edec765ef3e9e1a024416
strawmanbobi <strawmanbobi@irext.net>
2017-11-17 08:18:36 +0800

    completed repo creation and deletion actions

--- src/net/irext/AOSPGitInitiator.java
+++ src/net/irext/AOSPGitInitiator.java
@@ -15,6 +15,7 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Filename:       AOSPGitInitiator.java
@@ -37,27 +38,42 @@ public class AOSPGitInitiator {
              * 2. create sub-project repos
              * 3. TODO:
              */
-            if (5 != args.length) {
+            if (6 != args.length) {
                 System.out.println("invalid parameter");
                 System.out.println("Please call this method like java -jar AOSPGitInitiator.jar [token = 0] " +
                         "[source_repo_root] [dest_repo_root] [github base] [organization name]");
                 return;
             }
-            String githubToken = args[0];
-            String sourceRoot = args[1];
-            String destRoot = args[2];
-            String githubBase = args[3];
-            String orgName = args[4];
+            String action = args[0];
+            String githubToken = args[1];
+            String sourceRoot = args[2];
+            String destRoot = args[3];
+            String githubBase = args[4];
+            String orgName = args[5];
 
+            System.out.println("  action : " + action);
             System.out.println("  token : " + githubToken);
             System.out.println("  source : " + sourceRoot);
             System.out.println("  dest : " + destRoot);
             System.out.println("  github : " + githubBase);
             System.out.println("  org name : " + orgName);
             System.out.println("===============================");
-            AOSPGitInitiator agi = new AOSPGitInitiator(githubToken, sourceRoot, destRoot,
-                    githubBase, orgName);
-            agi.createRepo();
+            System.out.println();
+            System.out.println("Please check the parameters and type 'YES' to continue");
+            Scanner input = new Scanner(System.in);
+            if (input.next().equalsIgnoreCase("YES")) {
+                AOSPGitInitiator agi = new AOSPGitInitiator(githubToken, sourceRoot, destRoot,
+                        githubBase, orgName);
+                if (action.equals("add")) {
+                    agi.createRepo();
+                } else if (action.equals("delete")) {
+                    agi.deleteRepo();
+                } else {
+                    System.out.println("Invalid action : " + action);
+                }
+            } else {
+                System.out.println("Bye");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -68,6 +84,7 @@ public class AOSPGitInitiator {
     private static final String NODE_NAME_PROJECT = "project";
     private static final String NODE_NAME_REMOTE = "remote";
     private static final String NODE_NAME_DEFAULT = "default";
+    private static final String NODE_NAME_REPO_HOOKS = "repo-hooks";
 
     private static final String NODE_REMOTE_ATTR_NAME = "name";
     private static final String NODE_REMOTE_ATTR_FETCH = "fetch";
@@ -81,6 +98,9 @@ public class AOSPGitInitiator {
     private static final String NODE_PROJECT_ATTR_NAME = "name";
     private static final String NODE_PROJECT_ATTR_GROUP = "group";
 
+    private static final String NODE_REPO_HOOKS_IN_PROJECT = "in-project";
+
+    private String action;
     private String token;
     private String sourceRoot;
     private String destRoot;
@@ -99,19 +119,17 @@ public class AOSPGitInitiator {
         this.githubOrgURL = "ssh://git@" + githubBase + "/" + orgName + "/";
     }
 
-    private void createRepo() {
-        // step 1, build default.xml for manifest project
-        System.out.println("Step 1. create target default.xml");
-
-        String sourceManifestFilePath = sourceRoot + MANIFEST_XML;
-        String destManifestFilePath = destRoot + MANIFEST_XML;
+    private void deleteRepo() {
+        Document doc = null;
+        Element root = null;
+        NodeList topNodes = null;
         InputStream sourceInput = null;
 
+        String sourceManifestFilePath = sourceRoot + MANIFEST_XML;
+
+        List<String> projectList = new ArrayList<>();
+
         try {
-            Document doc = null;
-            Element root = null;
-            NodeList topNodes = null;
-            List<String> projectList = new ArrayList<>();
             DocumentBuilder domBuilder = DocumentBuilderFactory.newInstance()
                     .newDocumentBuilder();
 
@@ -119,6 +137,50 @@ public class AOSPGitInitiator {
             doc = domBuilder.parse(sourceInput);
             root = doc.getDocumentElement();
             topNodes = root.getChildNodes();
+            for (int topIndex = 0; topIndex < topNodes.getLength(); topIndex++) {
+                Node keyItemNode = topNodes.item(topIndex);
+
+                if (keyItemNode.getNodeType() == Node.ELEMENT_NODE) {
+                    if (keyItemNode.getNodeName().equals(NODE_NAME_PROJECT)) {
+                        NamedNodeMap attributes = keyItemNode.getAttributes();
+                        for (int attriIndex = 0; attriIndex < attributes.getLength(); attriIndex++) {
+                            Node item = attributes.item(attriIndex);
+                            if (item.getNodeName().equals(NODE_PROJECT_ATTR_NAME)) {
+                                String nameValue = item.getNodeValue();
+                                String newNameValue = nameValue.replaceAll("/", "_");
+                                projectList.add(newNameValue);
+                            }
+                        }
+                    }
+                }
+            }
+            DeleteRepoTask deleteRepoTask = new DeleteRepoTask(projectList, githubBase, orgName);
+            deleteRepoTask.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createRepo() {
+        // step 1, build default.xml for manifest project
+        System.out.println("Step 1. create target default.xml");
+        Document doc = null;
+        Element root = null;
+        NodeList topNodes = null;
+        InputStream sourceInput = null;
+
+        String sourceManifestFilePath = sourceRoot + MANIFEST_XML;
+        String destManifestFilePath = destRoot + MANIFEST_XML;
+
+        try {
+            DocumentBuilder domBuilder = DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder();
+
+            sourceInput = new FileInputStream(sourceManifestFilePath);
+            doc = domBuilder.parse(sourceInput);
+            root = doc.getDocumentElement();
+            topNodes = root.getChildNodes();
+            List<String> projectList = new ArrayList<>();
 
             int projectCount = 0;
             for (int topIndex = 0; topIndex < topNodes.getLength(); topIndex++) {
@@ -126,7 +188,6 @@ public class AOSPGitInitiator {
 
                 if (keyItemNode.getNodeType() == Node.ELEMENT_NODE) {
                     if (keyItemNode.getNodeName().equals(NODE_NAME_REMOTE)) {
-                        System.out.println("fetched element node of 'remote'");
                         // replace name and fetch attribute
                         NamedNodeMap attributes = keyItemNode.getAttributes();
                         for (int attriIndex = 0; attriIndex < attributes.getLength(); attriIndex++) {
@@ -143,7 +204,6 @@ public class AOSPGitInitiator {
                             }
                         }
                     } else if (keyItemNode.getNodeName().equals(NODE_NAME_DEFAULT)) {
-                        System.out.println("fetched element node of 'default'");
                         // replace name and fetch attribute
                         NamedNodeMap attributes = keyItemNode.getAttributes();
                         for (int attriIndex = 0; attriIndex < attributes.getLength(); attriIndex++) {
@@ -160,7 +220,6 @@ public class AOSPGitInitiator {
                             }
                         }
                     } else if (keyItemNode.getNodeName().equals(NODE_NAME_PROJECT)) {
-                        System.out.println("fetched element node of 'project'");
                         projectCount++;
                         NamedNodeMap attributes = keyItemNode.getAttributes();
                         for (int attriIndex = 0; attriIndex < attributes.getLength(); attriIndex++) {
@@ -173,8 +232,19 @@ public class AOSPGitInitiator {
                                 item.setNodeValue(newNameValue);
                             }
                         }
-                    } else {
-                        System.out.println("fetched element node of '" + keyItemNode.getNodeName() + "'");
+                    } else if (keyItemNode.getNodeName().equals(NODE_NAME_REPO_HOOKS)) {
+                        // update attribute of repo-hooks
+                        NamedNodeMap attributes = keyItemNode.getAttributes();
+                        for (int attriIndex = 0; attriIndex < attributes.getLength(); attriIndex++) {
+                            Node item = attributes.item(attriIndex);
+                            if (item.getNodeName().equals(NODE_REPO_HOOKS_IN_PROJECT)) {
+                                System.out.println("modify repo-hooks->in-project");
+                                String nameValue = item.getNodeValue();
+                                String newNameValue = nameValue.replaceAll("/", "_");
+                                newNameValue += ".git";
+                                item.setNodeValue(newNameValue);
+                            }
+                        }
                     }
                 }
             }
@@ -193,17 +263,8 @@ public class AOSPGitInitiator {
             System.out.println("Step 1 done");
 
             // step 2. delete sub-projects under github organization
-            // CreateRepoTask createRepoTask = new CreateRepoTask(projectList, githubBase, orgName);
-            // createRepoTask.start();
-            /*
-            for (String projectName : ) {
-                DeleteRepoTask deleteRepoTask = new DeleteRepoTask(projectName, githubBase, orgName);
-                deleteRepoTask.start();
-            }
-            */
-
-            // step 3. create sub-projects under github organization
-
+            CreateRepoTask createRepoTask = new CreateRepoTask(projectList, githubBase, orgName);
+            createRepoTask.start();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -222,11 +283,15 @@ public class AOSPGitInitiator {
         private String githubBase;
         private String orgName;
         final private Object lock = new Object();
+        private int createdProject;
+        private int totalProjectCount;
 
         CreateRepoTask(List<String> projectList, String githubBase, String orgName) {
             this.projectList = projectList;
             this.githubBase = githubBase;
             this.orgName = orgName;
+            this.createdProject = 0;
+            this.totalProjectCount = this.projectList.size();
         }
 
         @Override
@@ -246,7 +311,8 @@ public class AOSPGitInitiator {
                                                     @Override
                                                     public void onCreateFileSuccess(String response) {
                                                         System.out.println("create file to repo successfully : " +
-                                                                repoName);
+                                                                repoName + " (" + ++createdProject + "/" +
+                                                                totalProjectCount + ")");
                                                     }
 
                                                     @Override
@@ -265,12 +331,12 @@ public class AOSPGitInitiator {
 
                                     @Override
                                     public void onCreateRepoFailed() {
-                                        System.out.println("create repo failed");
+                                        System.out.println("create repo failed : " + repoName);
                                     }
 
                                     @Override
                                     public void onCreateRepoError() {
-                                        System.out.println("create repo error");
+                                        System.out.println("create repo error : " + repoName);
                                     }
                                 });
                     }
@@ -282,35 +348,42 @@ public class AOSPGitInitiator {
     }
 
     private class DeleteRepoTask extends Thread {
-        private String repoName;
+        private List<String> projectList;
         private String githubBase;
         private String orgName;
+        private int deletedProject;
+        private int totalProjectCount;
 
-        public DeleteRepoTask(String repoName, String githubBase, String orgName) {
-            this.repoName = repoName;
+        DeleteRepoTask(List<String> projectList, String githubBase, String orgName) {
+            this.projectList = projectList;
             this.githubBase = githubBase;
             this.orgName = orgName;
+            this.deletedProject = 0;
+            this.totalProjectCount = projectList.size();
         }
 
         @Override
         public void run() {
-            WebAPIs.getInstance().deleteRepo(repoName, githubBase, orgName, token,
-                    new WebAPICallbacks.DeleteRepoCallback() {
-                        @Override
-                        public void onDeleteRepoSuccess(String response) {
-                            System.out.println("delete repo successfully : " + response);
-                        }
+            for (String repoName : projectList) {
+                WebAPIs.getInstance().deleteRepo(repoName, githubBase, orgName, token,
+                        new WebAPICallbacks.DeleteRepoCallback() {
+                            @Override
+                            public void onDeleteRepoSuccess(String response) {
+                                System.out.println("delete repo successfully (" + ++deletedProject + "/" +
+                                        totalProjectCount + ")");
+                            }
 
-                        @Override
-                        public void onDeleteRepoFailed() {
-                            System.out.println("delete repo failed");
-                        }
+                            @Override
+                            public void onDeleteRepoFailed() {
+                                System.out.println("delete repo failed : " + repoName);
+                            }
 
-                        @Override
-                        public void onDeleteRepoError() {
-                            System.out.println("delete repo error");
-                        }
-                    });
+                            @Override
+                            public void onDeleteRepoError() {
+                                System.out.println("delete repo error : " + repoName);
+                            }
+                        });
+            }
         }
     }
 }
